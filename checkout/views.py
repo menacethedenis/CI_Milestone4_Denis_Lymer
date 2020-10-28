eac38a65ac0d4e73c9f44d223442c34cf4229d92
menacethedenis <denislymer@gmail.com>
2020-10-28 23:39:50 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HhNceJAHu7MS8QLq8c3Ry6GeFk06zWLYZYCiqyMnWIGIgxciWa3TKsl4KUawfpnOi2XfzsvgjLzuLWLUbhj7v6k00eQmEDBUe',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)

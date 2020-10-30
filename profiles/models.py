7d37eac6a787535efad3c3894f766df12ff53c07
menacethedenis <denislymer@gmail.com>
2020-10-30 00:59:48 +0000

    fixed typo on profiles models.py

--- profiles/models.py
+++ profiles/models.py
@@ -22,5 +22,5 @@ class UserProfile(models.Model):
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
-     UserProfile.objects.create(user=instance)
+        UserProfile.objects.create(user=instance)
     instance.userprofile.save()

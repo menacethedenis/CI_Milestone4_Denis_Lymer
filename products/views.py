c6adaef7df3967af1e00399778769e650a902a6c
menacethedenis <denislymer@gmail.com>
2020-10-30 21:43:35 +0000

    added functionality to superuser add/delete on site

--- products/views.py
+++ products/views.py
@@ -43,10 +43,11 @@ def all_products(request):
             if not query:
                 messages.error(request, "You didn't enter any search terms")
                 return redirect(reverse('products'))
-            
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
+
+            queries = Q(name__icontains=query) | \
+                Q(description__icontains=query)
             products = products.filter(queries)
-    
+
     current_sorting = f'{sort}_{direction}'
 
     context = {
@@ -73,7 +74,18 @@ def product_detail(request, product_id):
 
 def add_product(request):
     """ Add a product to the store """
-    form = ProductForm()
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. \
+                            Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+
     template = 'products/add_product.html'
     context = {
         'form': form,
